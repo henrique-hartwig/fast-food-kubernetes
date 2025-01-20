70cd512d95279403b6a5455f352542269573dfe5
Henrique Hartwig <henrique2805@gmail.com>
2025-01-20 11:46:28 -0300

    feat: add error treatment to orders and product controllers

--- src/infrastructure/http/ProductController.ts
+++ src/infrastructure/http/ProductController.ts
@@ -1,50 +1,86 @@
-import type { Request, Response } from 'express';
 import { ProductService } from '../../application/services/ProductService';
 import { PgProductRepository } from '../database/repository/PgProduct';
+
 
 const productRepository = new PgProductRepository();
 const productService = new ProductService(productRepository);
 
 export class ProductController {
   async createProduct(req: any, res: any) {
-    const { name, description, price, categoryId } = req.body;
-    await productService.createProduct(name, description, price, categoryId);
-    res.status(201).json({ message: 'Product created successfully!' });
+    try {
+      const { name, description, price, categoryId } = req.body;
+      await productService.createProduct(name, description, price, categoryId);
+      return res.status(201).json({ message: 'Product created successfully!' });
+    } catch (error: any) {
+      console.error('Error creating product:', error);
+      return res.status(500).json({ 
+        message: 'Internal server error',
+        error: error.message 
+      });
+    }
   }
 
   async getProductById(req: any, res: any) {
-    const product = await productService.getProductById(Number(req.params.id));
-    if (product) {
-      res.json(product);
-    } else {
-      res.status(404).json({ message: 'Product not found' });
+    try {
+      const product = await productService.getProductById(Number(req.params.id));
+      if (product) {
+        return res.status(200).json(product);
+      }
+      return res.status(404).json({ message: 'Product not found' });
+    } catch (error: any) {
+      console.error('Error searching product:', error);
+      return res.status(500).json({ 
+        message: 'Internal server error',
+        error: error.message 
+      });
     }
   }
 
   async getProductsByCategoryId(req: any, res: any) {
-    const products = await productService.getProductsByCategoryId(Number(req.params.categoryId));
-    res.json(products);
+    try {
+      const products = await productService.getProductsByCategoryId(Number(req.params.categoryId));
+      return res.status(200).json(products);
+    } catch (error: any) {
+      console.error('Error searching products by category:', error);
+      return res.status(500).json({ 
+        message: 'Internal server error',
+        error: error.message 
+      });
+    }
   }
 
   async getAllProducts(req: any, res: any) {
-    const products = await productService.getAllProducts();
-    res.json(products);
+    try {
+      const products = await productService.getAllProducts();
+      console.log('products', products);
+      return res.status(200).json(products);
+    } catch (error: any) {
+      console.error('Error searching products:', error);
+      return res.status(500).json({ 
+        message: 'Internal server error',
+        error: error.message 
+      });
+    }
   }
 
-  async updateProduct(req: Request, res: Response): Promise<void> {
+  async updateProduct(req: any, res: any) {
     try {
       const { id } = req.params;
       const productData = req.body;
       const updatedProduct = await productService.updateProduct(Number(id), productData);
-      res.json(updatedProduct);
+      return res.status(200).json(updatedProduct);
     } catch (error: any) {
-      res.status(400).json({ error: error.message });
+      return res.status(400).json({ error: error.message });
     }
   }
 
-  async deleteProduct(req: Request, res: Response): Promise<void> {
-    const { id } = req.params;
-    await productService.deleteProduct(Number(id));
-    res.status(200).json({ message: 'Product deleted successfully!' });
+  async deleteProduct(req: any, res: any) {
+    try {
+      const { id } = req.params;
+      await productService.deleteProduct(Number(id));
+      return res.status(200).json({ message: 'Product deleted successfully!' });
+    } catch (error: any) {
+      return res.status(400).json({ error: error.message });
+    }
   }
 }
