c79147e121dee7d16d8d05738f6fab049c3f0d8c
Henrique Hartwig <henrique2805@gmail.com>
2025-01-21 19:34:10 -0300

    feat: create methods to decouple repositpries from prisma

--- src/external/database/DatabaseConnection.ts
+++ src/external/database/DatabaseConnection.ts
@@ -1,21 +1,39 @@
 import { Prisma, PrismaClient } from '@prisma/client';
 
+export interface IBaseDatabaseOperations<T> {
+  findAll(): Promise<T[]>;
+  findById(id: string | number): Promise<T | null>;
+  create(data: any): Promise<T>;
+  update(id: string | number, data: any): Promise<T>;
+  delete(id: string | number): Promise<void>;
+}
+
 export interface IDatabaseConnection extends PrismaClient {
     close(): void;
     open(): void;
     query(query: string): Promise<any[]>;
+    getRepository<T>(entity: string): IBaseDatabaseOperations<T>;
 }
 
 export default class DatabaseConnection extends PrismaClient implements IDatabaseConnection {
-  readonly connection: IDatabaseConnection;
+  readonly connection: PrismaClient;
 
   constructor() {
     super();
     this.connection = this;
   }
 
-  async query(query: string): Promise<any[]> {
-    return this.connection.$queryRaw<Array<any>>(Prisma.sql`${query}`);
+  async query(queryTemplate: string, params?: any[]): Promise<any[]> {
+    if (!params || params.length === 0) {
+      return this.connection.$queryRawUnsafe(queryTemplate);
+    }
+
+    const numberedQuery = queryTemplate.replace(/\?/g, (_, i) => `$${i + 1}`);
+
+    return this.connection.$queryRaw.apply(
+      this.connection,
+      [Prisma.sql([numberedQuery, ...params])]
+    ) as Promise<any[]>;
   }
 
   open(): void {
@@ -24,5 +42,36 @@ export default class DatabaseConnection extends PrismaClient implements IDatabas
 
   close(): void {
     this.connection.$disconnect();
+  }
+
+  getRepository<T>(entity: string): IBaseDatabaseOperations<T> {
+    const prismaEntity = this.connection[entity as keyof PrismaClient] as any;
+
+    return {
+      findAll: async () => {
+        return prismaEntity.findMany();
+      },
+      findById: async (id: string | number) => {
+        return prismaEntity.findUnique({
+          where: { id }
+        });
+      },
+      create: async (data: any) => {
+        return prismaEntity.create({
+          data
+        });
+      },
+      update: async (id: string | number, data: any) => {
+        return prismaEntity.update({
+          where: { id },
+          data
+        });
+      },
+      delete: async (id: string | number) => {
+        await prismaEntity.delete({
+          where: { id }
+        });
+      }
+    };
   }
 }
